993281f3054c04fa49c80e88a3af202fb5a87489
harrisoncrazy <Tech@C422IT35.gbc.local>
2017-01-17 15:34:25 -0500

    starting grid for snake

--- Assets/Scripts/snakeHandler.cs
+++ Assets/Scripts/snakeHandler.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class snakeHandler : MonoBehaviour {
 
-
+	public TileListCheck.movementIndex[] movementIndex = new TileListCheck.movementIndex[28];
 
 	// Use this for initialization
 	void Start () {
-		
+		//movementIndex = new TileListCheck.movementIndex[28];
+
+		for (int i = 0; i <= movementIndex.Length-1; i++) {
+			Debug.Log (i);
+			movementIndex [0] = TileListCheck.Instance.CheckTileMovement (i);
+		}
 	}
 	
 	// Update is called once per frame
