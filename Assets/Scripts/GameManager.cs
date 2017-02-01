cd42a6e1d55f69f429e11f0de580cc0ad3ef8c5c
harrisoncrazy <harrisoncrawford16@yahoo.ca>
2017-02-01 17:09:18 -0500

    level loading and continuation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,7 @@ public class GameManager : MonoBehaviour {
 
 	public int totalConvertedTiles = 0;
 	public int totalScore = 0;
+	private int levelNum = 1;
 
 	//ending values
 	public bool inputDelay = true;
@@ -48,6 +49,10 @@ public class GameManager : MonoBehaviour {
 		player.GetComponent<playerHandler> ().currentRow = 3;
 		player.transform.position = new Vector3 (GameObject.Find ("tile" + player.GetComponent<playerHandler> ().currentTile + "Base").transform.position.x, (GameObject.Find ("tile" + player.GetComponent<playerHandler> ().currentTile + "Base").transform.position.y + 0.15f));
 
+	}
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
 	}
 	
 	// Update is called once per frame
@@ -96,6 +101,29 @@ public class GameManager : MonoBehaviour {
 
 	IEnumerator WinGame() {
 		yield return new WaitForSeconds (3.0f);
-		SceneManager.LoadScene ("level1");
+		levelNum++;
+		SceneManager.LoadScene ("level" + levelNum);
+		totalConvertedTiles = 0;
+		isWinning = false;
+		isEnding = false;
+		isFalling = false;
+
+		StartCoroutine ("SpawnPlayer");
+		/*
+		GameObject player = Instantiate (playerPrefab, transform.position, Quaternion.identity);
+		player.GetComponent<playerHandler> ().currentTile = 5;
+		player.GetComponent<playerHandler> ().currentRow = 3;
+		player.transform.position = new Vector3 (GameObject.Find ("tile" + player.GetComponent<playerHandler> ().currentTile + "Base").transform.position.x, (GameObject.Find ("tile" + player.GetComponent<playerHandler> ().currentTile + "Base").transform.position.y + 0.15f));
+		*/
+	}
+
+	IEnumerator SpawnPlayer() {
+		yield return new WaitForSeconds (0.5f);
+
+		GameObject player = Instantiate (playerPrefab, transform.position, Quaternion.identity);
+		player.GetComponent<playerHandler> ().currentTile = 5;
+		player.GetComponent<playerHandler> ().currentRow = 3;
+		player.transform.position = new Vector3 (GameObject.Find ("tile" + player.GetComponent<playerHandler> ().currentTile + "Base").transform.position.x, (GameObject.Find ("tile" + player.GetComponent<playerHandler> ().currentTile + "Base").transform.position.y + 0.15f));
+		isEnding = false;
 	}
 }
---
+++ Assets/Scripts/mainMenuHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenuHandler : MonoBehaviour {
+
+	public string levelName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void loadLevel() {
+		SceneManager.LoadScene(levelName);
+		Time.timeScale = 1;
+	}
+
+	public void Exit() {
+		Time.timeScale = 1;
+		Application.Quit ();
+	}
+}
