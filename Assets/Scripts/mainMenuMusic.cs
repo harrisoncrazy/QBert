57448b74f9a07b7ab80eae4f7798442d34477930
harrisoncrazy <harrisoncrawford16@yahoo.ca>
2017-04-20 10:53:50 -0400

    final submission

---
+++ Assets/Scripts/mainMenuMusic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenuMusic : MonoBehaviour {
+
+	private AudioSource audioMain;
+
+	public AudioClip[] audioList;
+
+	// Use this for initialization
+	void Start () {
+		audioMain = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (audioMain.isPlaying == false) {
+			int rando = Random.Range (0, audioList.Length);
+
+			audioMain.PlayOneShot (audioList [rando]);
+		}
+	}
+}
